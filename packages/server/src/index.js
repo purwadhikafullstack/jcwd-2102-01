ed287b7d6964d0f4b6145a4b1430eba88c09c295
Purwadhika Fullstack <93514408+purwadhikafullstack@users.noreply.github.com>
2022-09-14 16:03:31 +0700

    Update index.js

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const { join } = require('path');
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.CUSTOM_PORT || 8000;
 const app = express();
 app.use(cors());
 
